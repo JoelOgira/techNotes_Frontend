c5fcfad990d9fa6f4e2a8a1bd70da14703fa838f
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-03 17:56:11 +0300

    Created DashFooter component

---
+++ src/components/DashFooter.jsx
@@ -0,0 +1,32 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+
+const DashFooter = () => {
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
+    const thisYear = new Date().getFullYear();
+    const onGoHomeClicked = () => navigate('/dash');
+
+    let goHomeButton = null;
+    if (pathname !== '/dash') {
+        goHomeButton = (
+            <button
+                className="border-2"
+                title="Home"
+                onClick={onGoHomeClicked}
+            >
+                <FontAwesomeIcon icon={faHouse} />
+            </button>
+        )
+    }
+
+    return (
+        <footer>
+            {goHomeButton}
+            <p>Copyright &copy; techNotes {thisYear}</p>
+        </footer>
+    )
+}
+
+export default DashFooter;
