389d28be27b2b813142429d5df18008d5896cc29
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-03 17:57:18 +0300

    Created DashLayout component

---
+++ src/components/DashHeader.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const DashHeader = () => {
+    return (
+        <header>
+            <div className="">
+                <Link to="/dash/notes">
+                    <h1>techNotes</h1>
+                </Link>
+                <nav className="flex justify-between items-center">
+                    <div className=""></div>
+                    <div className=""></div>
+                    <div className="">
+                        <p>Current User:</p>
+                        <p>Status:</p>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default DashHeader;
