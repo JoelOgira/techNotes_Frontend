d7179f5c31ab859b9f81ec4f0a1f844e20c8a315
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-06 21:16:14 +0300

    Created welcome component

---
+++ src/features/auth/Welcome.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Welcome = () => {
+
+    const date = new Date();
+    const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date);
+
+    return (
+        <section className="flex justify-start items-center bg-opacity-20">
+            <p>{today}</p>
+            <h1 className="text-2xl font-semibold text-center underline">Welcome</h1>
+            <p><Link to="/dash/notes">View techNotes</Link></p>
+            <p><Link to="/dash/users">View user Settings</Link></p>
+        </section>
+    )
+}
+
+export default Welcome;
