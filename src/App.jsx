347544c41f61c041689c5ab260d5731bfa9c5bbf
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-07 20:28:31 +0300

    Added new styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import UsersList from "./features/users/UsersList";
 
 export default function App() {
   return (
-    <div className="bg-black">
+    <div className="bg-black text-white">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Public />} />
