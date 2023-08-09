300ead0ce3fde4b964d0c3738ae85419e3c2dd62
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-09 20:14:13 +0300

    Added new styling

--- src/features/auth/Welcome.jsx
+++ src/features/auth/Welcome.jsx
@@ -6,9 +6,9 @@ const Welcome = () => {
     const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date);
 
     return (
-        <section className="flex flex-col justify-start items-start bg-opacity-20">
+        <section className="flex flex-col space-y-6 justify-start items-start bg-opacity-20">
             <p>{today}</p>
-            <h1 className="text-2xl font-semibold text-center underline">Welcome</h1>
+            <h1 className="text-2xl font-semibold underline">Welcome</h1>
             <p><Link to="/dash/notes">View techNotes</Link></p>
             <p><Link to="/dash/users">View user Settings</Link></p>
         </section>
