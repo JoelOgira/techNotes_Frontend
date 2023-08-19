50e9ef00c751a82908b2ee10821bc3b2a061f114
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-19 20:39:22 +0300

    Updated styling

--- src/components/DashHeader.jsx
+++ src/components/DashHeader.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const DashHeader = () => {
     return (
         <header>
-            <div className="flex justify-between items-center mb-6">
+            <div className="flex justify-between items-center px-5 md:px-0 mb-3 py-8">
                 <Link to="/dash/notes">
-                    <h1 className="font-bold text-2xl">techNotes</h1>
+                    <h1 className="font-bold text-2xl underline">techNotes</h1>
                 </Link>
                 <nav className="flex justify-between items-center">
                     <div className=""></div>
