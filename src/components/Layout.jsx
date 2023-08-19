e5f361695230aef8a4aeb61f64c4902cab196bfc
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-19 20:39:46 +0300

    Updated styling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
     return (
-        <div className="mx-auto container">
+        <div className="mx-auto container md:w-3/4">
             <Outlet />
         </div>
     )
