176a145ef975854bded0649adfa797c5bf5a434e
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-21 14:26:09 +0300

    Added content

--- src/components/Public.jsx
+++ src/components/Public.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Public = () => {
     const content = (
         <section>
-            
+            This is the welcome section
         </section>
     )
 
