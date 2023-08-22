20b167a4bf05d04e14619029be57c7a51ba2ea94
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-22 20:27:43 +0300

    Added horizontal rule

--- src/components/DashHeader.jsx
+++ src/components/DashHeader.jsx
@@ -3,18 +3,23 @@ import { Link } from "react-router-dom";
 const DashHeader = () => {
     return (
         <header>
-            <div className="flex justify-between items-center px-5 md:px-0 mb-3 py-8">
-                <Link to="/dash/notes">
-                    <h1 className="font-bold text-2xl underline">techNotes</h1>
-                </Link>
-                <nav className="flex justify-between items-center">
-                    <div className=""></div>
-                    <div className=""></div>
-                    <div className="">
-                        <p>Current User:</p>
-                        <p>Status:</p>
-                    </div>
-                </nav>
+            <div className="flex flex-col">
+                <div className="flex justify-between items-center mb-3 py-6">
+                    <Link to="/dash/notes">
+                        <h1 className="font-bold text-2xl underline">techNotes</h1>
+                    </Link>
+                    <nav className="flex justify-between items-center">
+                        <div className=""></div>
+                        <div className=""></div>
+                        <div className="">
+                            <p>Current User:</p>
+                            <p>Status:</p>
+                        </div>
+                    </nav>
+                </div>
+                <div className="mb-8">
+                    <hr className="border" />
+                </div>
             </div>
         </header>
     )
