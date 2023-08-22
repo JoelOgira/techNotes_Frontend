6b7e690a99f7a4e75048619d0cdeb675e7ae6224
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-22 20:30:29 +0300

    Modified the content

--- src/features/auth/Welcome.jsx
+++ src/features/auth/Welcome.jsx
@@ -6,11 +6,11 @@ const Welcome = () => {
     const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date);
 
     return (
-        <section className="flex flex-col space-y-6 justify-start items-start px-5 md:px-0">
+        <section className="flex flex-col space-y-6 justify-start items-start">
             <p className="max-w-xs">{today}</p>
             <h1 className="text-2xl font-semibold underline">Welcome</h1>
             <p><Link to="/dash/notes">View techNotes</Link></p>
-            <p><Link to="/dash/users">View user Settings</Link></p>
+            <p><Link to="/dash/users">View users</Link></p>
         </section>
     )
 }
