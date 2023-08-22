7e3e2b7ddc91ea3fb226e211295b9c20dce232a8
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-22 20:29:26 +0300

    Added content to the public component

--- src/components/Public.jsx
+++ src/components/Public.jsx
@@ -2,12 +2,38 @@ import { Link } from "react-router-dom";
 
 const Public = () => {
     const content = (
-        <section>
-            This is the welcome section
-        </section>
+        <>
+            <section className="flex flex-col space-y-6">
+                <h1 className="text-2xl font-semibold font-mono pt-8 pb-2">
+                    Welcome to Zephyrus Labs!
+                </h1>
+
+                <hr className="border" />
+
+                <p>Located in Beautiful Elysium City, Zephyrus provides the best specialists for your augmentation needs.</p>
+
+                <div className="flex flex-col">
+                    <p>Zephyrus Labs</p>
+                    <p>555 Augmentation Verizon</p>
+                    <p>Elysium City, ISS 12345</p>
+                    <p>(666) 666-666</p>
+                </div>
+
+                <p>Compadre: G14</p>
+            </section>
+
+            <section className="top-full sticky">
+                <hr className="border" />
+                <p className="py-6">
+                    <Link to='/login'>
+                        Employee login
+                    </Link>
+                </p>
+            </section>
+        </>
     )
 
-    return content
+    return content;
 }
 
 export default Public;
